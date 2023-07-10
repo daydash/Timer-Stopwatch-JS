c683f1253a4ba1428481037c037c9a282cc4a1aa
daydash <thedaydash@gmail.com>
2023-07-10 17:39:57 +0530

    2_rGxA2qNL_added_timer_layout

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const stopwatchStart = document.querySelector(".stopwatchStart");
 const time = document.querySelector(".time");
-const btnElement = document.querySelectorAll(".btn");
+const stopwatchBtnElement = document.querySelectorAll(".stopwatchBtn");
 
 const hrElement = document.getElementById("hr");
 const minElement = document.getElementById("min");
@@ -12,7 +12,7 @@ hr = min = sec = ms = 0;
 
 timer = false;
 
-btnElement.forEach((btn) => {
+stopwatchBtnElement.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     switch (e.target.name) {
       case "start":
@@ -96,4 +96,40 @@ const stopwatch = () => {
       stopwatch();
     }, 10);
   }
+};
+
+// Timer
+
+const startCountdown = () => {
+  console.log(document.getElementById("timerInput").getAttribute("color"));
+  const targetDate = new Date("2023-12-31T23:59:59").getTime();
+  const currentDate = new Date().getTime();
+  const timeRemaining = targetDate - currentDate;
+
+  const daysElement = document.getElementById("daysTimer");
+  const hoursElement = document.getElementById("hoursTimer");
+  const minutesElement = document.getElementById("minutesTimer");
+  const secondsElement = document.getElementById("secondsTimer");
+
+  let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+  hours = String(hours).padStart(2, "0");
+  minutes = String(minutes).padStart(2, "0");
+  seconds = String(seconds).padStart(2, "0");
+
+  daysElement.textContent = days;
+  hoursElement.textContent = hours;
+  minutesElement.textContent = minutes;
+  secondsElement.textContent = seconds;
+
+  // TODO: Implement the countdown logic to update the timer every second
+};
+
+const startTimer = () => {
+  setInterval(startCountdown, 1000);
+  console.log("yash");
 };
